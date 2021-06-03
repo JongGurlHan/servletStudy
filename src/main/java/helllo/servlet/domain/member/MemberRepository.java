f0cc532e25a57e294e23ca5143de5b15e13dbc94
JG HAN <gojkhan13@gmail.com>
2021-06-04 02:28:51 +0900

    0603 회원관리 웹애플리케이션(서블릿)

---
+++ src/main/java/helllo/servlet/domain/member/MemberRepository.java
@@ -0,0 +1,42 @@
+package helllo.servlet.domain.member;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MemberRepository {
+
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+
+    private static final MemberRepository instance = new MemberRepository();
+
+    public static MemberRepository getInstance(){  //무조건 getInstance로만 조회가 된다.
+        return instance;
+    }
+    private MemberRepository(){} // 싱글톤으로 만들때는 private으로 생성자 만들어서 아무나 생성못하게 한다.
+
+
+    public Member save(Member member){
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    public Member findById(Long id){
+        return store.get(id);
+    }
+
+    public List<Member>findAll(){
+        return new ArrayList<>(store.values()); //store에 있는 모든 값들을 꺼내서 새로운 Arraylist에 담에서 return
+    }
+
+    public void clearStore(){
+        store.clear();
+    }
+
+
+
+
+}
---
+++ src/test/java/helllo/servlet/domain/member/MemberRepositoryTest.java
@@ -0,0 +1,51 @@
+package helllo.servlet.domain.member;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class MemberRepositoryTest {
+
+    MemberRepository memberRepository = MemberRepository.getInstance();
+    //싱글톤이기 때문에 new하지 않는다.
+    //스프링 쓰면 스프링 자체에서 싱글톤 보장해준다.
+
+    @AfterEach // 테스트 끝날때 마다 테스트 초기화
+    void afterEach(){
+        memberRepository.clearStore();
+    }
+
+    @Test
+    void save(){
+        //given
+        Member member = new Member("hello", 20);
+
+        //when
+        Member savedMember = memberRepository.save(member);
+
+        //then
+        Member findMember = memberRepository.findById(savedMember.getId());
+        assertThat(findMember).isEqualTo(savedMember);
+    }
+
+    @Test
+    void findAll(){
+        //given
+        Member member1 = new Member("member1", 20);
+        Member member2 = new Member("member2", 25);
+
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+        
+        //when
+        List<Member> result = memberRepository.findAll();
+
+        //then
+        assertThat(result.size()).isEqualTo(2);
+        assertThat(result).contains(member1, member2);
+    }
+}
