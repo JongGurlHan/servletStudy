c61b52e9fa7aeda31e03dd5961e517ff49cb82b1
JG HAN <gojkhan13@gmail.com>
2021-06-06 21:56:49 +0900

    0606 View분리(v2)

---
+++ src/main/java/helllo/servlet/web/frontcontroller/v2/FrontControllerServletV2.java
@@ -0,0 +1,43 @@
+package helllo.servlet.web.frontcontroller.v2;
+
+import helllo.servlet.web.frontcontroller.MyView;
+import helllo.servlet.web.frontcontroller.v1.ControllerV1;
+
+import helllo.servlet.web.frontcontroller.v2.controller.MemberFormControllerV2;
+import helllo.servlet.web.frontcontroller.v2.controller.MemberListControllerV2;
+import helllo.servlet.web.frontcontroller.v2.controller.MemberSaveControllerV2;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet(name = "frontControllerServletV2", urlPatterns = "/front-controller/v2/*") ///front-controller/v1 을 포함한 하위 요청은 이 서블릿에서 받아드린다.
+public class FrontControllerServletV2 extends HttpServlet {
+    private Map<String, ControllerV2> controllerMap = new HashMap<>();
+
+    public FrontControllerServletV2() {
+        controllerMap.put("/front-controller/v2/members/new-form", new MemberFormControllerV2());
+        controllerMap.put("/front-controller/v2/members/save", new MemberSaveControllerV2());
+        controllerMap.put("/front-controller/v2/members", new MemberListControllerV2());
+    }
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String requestURI = request.getRequestURI();
+        ControllerV2 controller = controllerMap.get(requestURI);
+
+        if (controller == null) {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+
+        MyView view = controller.process(request, response);
+        view.render(request,response);
+    }
+}
