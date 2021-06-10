a96788770606fa2524864d092a473f7f41335c9c
JG HAN <gojkhan13@gmail.com>
2021-06-11 02:52:35 +0900

    0611 컨트롤러 변경(v5)-2

--- src/main/java/helllo/servlet/web/frontcontroller/v5/FrontControllerServletV5.java
+++ src/main/java/helllo/servlet/web/frontcontroller/v5/FrontControllerServletV5.java
@@ -11,6 +11,7 @@ import helllo.servlet.web.frontcontroller.v4.controller.MemberFormControllerV4;
 import helllo.servlet.web.frontcontroller.v4.controller.MemberListControllerV4;
 import helllo.servlet.web.frontcontroller.v4.controller.MemberSaveControllerV4;
 import helllo.servlet.web.frontcontroller.v5.adapter.ControllerV3HandlerAdapter;
+import helllo.servlet.web.frontcontroller.v5.adapter.ControllerV4HandlerAdapter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -39,10 +40,16 @@ public class FrontControllerServletV5 extends HttpServlet {
         handlerMappingMap.put("/front-controller/v5/v3/members/new-form", new MemberFormControllerV3());
         handlerMappingMap.put("/front-controller/v5/v3/members/save", new MemberSaveControllerV3());
         handlerMappingMap.put("/front-controller/v5/v3/members", new MemberListControllerV3());
+
+        //v4추가
+        handlerMappingMap.put("/front-controller/v5/v4/members/new-form", new MemberFormControllerV4());
+        handlerMappingMap.put("/front-controller/v5/v4/members/save", new MemberSaveControllerV4());
+        handlerMappingMap.put("/front-controller/v5/v4/members", new MemberListControllerV4());
     }
 
     private void initHandlerAdapters() {
         handlerAdapters.add(new ControllerV3HandlerAdapter());
+        handlerAdapters.add(new ControllerV4HandlerAdapter());
     }
 
     @Override
