35f7536cb667c6d310338795cf8bb159a128b8f6
JG HAN <gojkhan13@gmail.com>
2021-06-11 00:00:10 +0900

    0611 컨트롤러 변경(v5)

---
+++ src/main/java/helllo/servlet/web/frontcontroller/v5/FrontControllerServletV5.java
@@ -0,0 +1,19 @@
+package helllo.servlet.web.frontcontroller.v5;
+
+import helllo.servlet.web.frontcontroller.v4.ControllerV4;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@WebServlet(name ="frontControllerServletV5", urlPatterns = "/front-controller/v5/*")
+public class FrontControllerServletV5 extends HttpServlet {
+
+    private final Map<String, Object> handlerMappingMap = new HashMap<>();
+    private final List<MyHandlerAdapter> handlerAdapters = new ArrayList<>();
+
+
+}
